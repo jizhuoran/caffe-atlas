5808ee4334413410589e7d89fabf372ea9a8574d
jizhuoran <jizr@connect.hku.hk>
2020-07-21 16:25:18 +0800

    auto push

--- examples/mnist/onram_aicore.sh
+++ examples/mnist/onram_aicore.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 set -e
 
-./build/tools/caffe train --solver=examples_ram/mnist/onram_lenet_aicore_solver.prototxt $@
+./build/tools/caffe train --solver=examples_ram/mnist/onram_lenet_train_test.prototxt $@
